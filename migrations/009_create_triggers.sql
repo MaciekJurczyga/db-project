705629e34f5e9799d2c56199a4802457d30fb00f
Maciej <maciekjur123@gmail.com>
2025-02-01 20:11:23 +0100

    added new triggers and functions

--- migrations/009_create_triggers.sql
+++ migrations/009_create_triggers.sql
@@ -27,3 +27,38 @@ CREATE TRIGGER check_overdue_invoice_trigger
 BEFORE UPDATE ON Invoices
 FOR EACH ROW
 EXECUTE FUNCTION check_overdue_invoice();
+
+CREATE OR REPLACE FUNCTION check_appointment_time()
+RETURNS TRIGGER AS $$
+BEGIN
+  IF NEW.appointment_time < '08:00' OR NEW.appointment_time > '16:00' THEN
+    RAISE EXCEPTION 'Appointments can only be scheduled between 8:00 and 16:00.';
+  END IF;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER check_appointment_time_trigger
+BEFORE INSERT OR UPDATE ON Appointments
+FOR EACH ROW
+EXECUTE FUNCTION check_appointment_time();
+
+
+CREATE OR REPLACE FUNCTION calculate_invoice_total()
+RETURNS TRIGGER AS $$
+BEGIN
+    SELECT SUM(s.price) 
+    INTO NEW.total_amount
+    FROM Appointment_Services aps
+    JOIN Services s ON aps.service_id = s.service_id
+    WHERE aps.appointment_id = NEW.appointment_id;
+        
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER calculate_invoice_total_trigger
+BEFORE INSERT ON Invoices
+FOR EACH ROW
+EXECUTE FUNCTION calculate_invoice_total();
--- migrations/010_create_functions.sql
+++ migrations/010_create_functions.sql
@@ -30,3 +30,36 @@ BEGIN
     WHERE a.doctor_id = doctor_id_input;
 END;
 $$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION get_upcoming_appointments(days_ahead INT)
+RETURNS TABLE (
+    patient_name VARCHAR(50),
+    patient_surname VARCHAR(50),
+    appointment_date DATE,
+    appointment_time TIME
+) AS $$
+BEGIN
+  RETURN QUERY
+  SELECT p.name, p.surname, a.appointment_date, a.appointment_time
+  FROM Patients p
+  JOIN Appointments a ON p.patient_id = a.patient_id
+  WHERE a.appointment_date BETWEEN CURRENT_DATE AND CURRENT_DATE + INTERVAL '1 day' * days_ahead
+  ORDER BY a.appointment_date, a.appointment_time;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+CREATE OR REPLACE FUNCTION count_doctor_appointments(doctor_id_input INT, start_date DATE, end_date DATE)
+RETURNS INT AS $$
+DECLARE
+  appointment_count INT;
+BEGIN
+  SELECT COUNT(*) INTO appointment_count
+  FROM Appointments
+  WHERE doctor_id = doctor_id_input
+    AND appointment_date BETWEEN start_date AND end_date;
+
+  RETURN appointment_count;
+END;
+$$ LANGUAGE plpgsql;
